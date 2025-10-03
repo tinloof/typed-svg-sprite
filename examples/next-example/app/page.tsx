22fb5c45716471f5cb47210b51a5fe01e4094011
LHDi <elhadi98@gmail.com>
2025-10-03 13:52:25 +0100

    Add dynamic export to layout and page components in next-example

--- examples/next-example/app/page.tsx
+++ examples/next-example/app/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 export default function Sprite() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-purple-50 py-12">
