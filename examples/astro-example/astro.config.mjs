33d04045f8f806d76e9a78e271f5114fbca5a78c
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2026-02-02 17:15:08 +0100

    chore: removed astro react integration

--- examples/astro-example/astro.config.mjs
+++ examples/astro-example/astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from "astro/config";
 import { svgSprite } from "@tinloof/typed-svg-sprite/astro";
 
-import react from "@astrojs/react";
-
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   integrations: [
     svgSprite({
       inputDir: "../../shared/icons",
-    }),
-    react(),
+    })
   ],
 
   vite: {
